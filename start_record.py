57e4712f70155d937b95d346be8bfc26789728f5
이희근 <v2msyo@naver.com>
2023-08-02 16:00:48 +0900

    update
    유니티 프로젝트의 파일이 너무 커 분할압축하여 올림.

---
+++ start_record.py
@@ -0,0 +1,29 @@
+
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+import time
+
+file_path = "record_data.bag"
+pipeline = rs.pipeline()
+config = rs.config()
+
+width, height = 1280, 720
+config.enable_record_to_file(file_path)
+config.enable_stream(rs.stream.depth, width, height, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, width, height, rs.format.bgr8, 30)
+
+pipeline.start(config)
+
+try:
+    print(time.time())
+    while True:
+        frames = pipeline.wait_for_frames()
+
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+finally:
+
+    # Stop streaming
+    pipeline.stop()
