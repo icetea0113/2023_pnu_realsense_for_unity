1b287be449c6f457ffeded6f83b1f547fd3cadbb
이희근 <v2msyo@naver.com>
2023-08-03 17:57:14 +0900

    1234

--- extract_data.py
+++ extract_data.py
@@ -16,6 +16,7 @@ profile = pipeline.start(config)
 depth_sensor = profile.get_device().first_depth_sensor()    # depth sensor에 대한 것들을 얻자
 depth_scale = depth_sensor.get_depth_scale()                # 깊이 센서의 깊이 스케일 얻음
 
+print(depth_scale)
 mp_pose = mp.solutions.pose
 pose = mp_pose.Pose()
 
@@ -47,18 +48,20 @@ with open(file_path, "w") as f:
                 for i, landmark in enumerate(results.pose_landmarks.landmark):
                     timestamp = color_frame.get_timestamp()
 
-                    if 0 <= landmark.x < 1 and 0 <= landmark.y < 1:
-                        image_point = [int(landmark.x * color_image.shape[1]), int(landmark.y * color_image.shape[0])]
-                        print(image_point)
+                    if 0 <= landmark.x <= 1 and 0 <= landmark.y <= 1:
+                        image_point = [int(round(landmark.x * color_image.shape[1])), int(round(landmark.y * color_image.shape[0]))]
                         depth_value = depth_image[image_point[1], image_point[0]]
                     else:
                         depth_value = None
 
                     f.write(f"{timestamp}, {i}, {landmark.x}, {landmark.y}, {depth_value}\n")
+                    #f.write(f"{timestamp}, {i}, {landmark.x}, {landmark.y}, {landmark.z}\n")
                     # timestamp -> mili second
             print("-----------------------------------------")
-            # cv2.imshow('RGB Image', annotated_image)
-            # cv2.imshow('Depth Image', depth_image)
+            cv2.imshow('RGB Image', annotated_image)
+            cv2.imshow('Depth Image', depth_image)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
     except Exception as e:
         print(f"An error occurred: {e}")
 
